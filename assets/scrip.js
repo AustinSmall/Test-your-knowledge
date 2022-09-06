6bedb5fe67f03661f796e6a7cde9ee497af9b307
austinsmall <Austin1540@gmail.com>
2022-09-05 21:52:08 -0400

    showquestions

--- assets/scrip.js
+++ assets/scrip.js
@@ -50,9 +50,29 @@ var questions =[
     },
     correctanswer: "C",
 },
-]
+];
 
-function generatequiz( questions, finis)
+function showquestions (questions, quizContainer) {
+    var output = [];
+    var answers;
+    
+    for(var i=0; i<questions.length; i++) {
+        answers = [];
+        for(letter in questions[i].answers){
+            answers.push(
+                '<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+            );
+        }
+        output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+    quizContainer.innerhtml= output.join('')
+    
+}
 
-//this
-//
