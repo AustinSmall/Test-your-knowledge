6040d23cba9c84c9e98a1c556861f67a273bdc18
Austin Small <Austin1540@gmail.com>
2022-10-09 23:26:58 -0400

    final commit before overhaul

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions =[ 
+    {
+        numb: 1,
+        question: "Inside which HTML elelment do we put the JavaScript?",
+        options: [
+           "js",
+           "script",
+           "javascript",
+           "jscript",
+       ],
+       answer:"script"
+    },
+{
+    numb: 2,
+    question: "Which of the following function of numb object returns the numb's value?",
+    options: [
+         "toString()",
+         "valueOf()",
+         "toLocaleString()",
+         "toPrecision()",
+    ],
+    answer: "valueOf()"
+},
+{
+    numb: 3,
+    question: "Which of the followong function of Array object reprsents the source code of an object",
+    options: [
+         "toSource",
+         "splice",
+         "toString",
+         "Unshift",
+    ],
+    answer: "toSource",
+},
+{
+    numb: 4,
+    question : "Which built-in method returns the characters in a string beginning at the specified location?",
+    options: [
+         "substr()",
+         "getSubstring",
+         "slice",
+         "None of the above",
+    ],
+    answer: "substr()",
+},
+{
+    numb: 5,
+    question: "Which of the following function of Array object returns the first (least) index of an element within the array equal to the specified value, or -1 if none is found?",
+    options: [
+         "join()",
+         "lastIndexOf()",
+         "indexOf()",
+         "map()",
+    ],
+    answer: "indexOf()",
+},
+]
+
